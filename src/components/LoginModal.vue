20b4f31cd9993c4d8aab6a1eade5e1169ad40661
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-09-17 14:41:49 +0200

    Login and Register modals were fully developed

---
+++ src/components/LoginModal.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="flex h-screen justify-center items-center backdrop-blur-sm bg-white/30">
+    <div
+      class="relative flex h-auto max-w-md flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white border-2 rounded-xl border-gray-300/30 shadow-md"
+    >
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          class="mx-auto h-15 w-auto"
+          src="../assets/img/logos/BlueLogo.svg"
+          alt="Your Company"
+        />
+      </div>
+
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="#" method="POST">
+          <div>
+            <div class="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autocomplete="email"
+                placeholder="Email"
+                class="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-blueFunko-500 focus:ring-blueFunko-300 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div class="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                placeholder="Password"
+                class="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-blueFunko-500 focus:ring-blueFunko-300 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              class="flex w-full justify-center rounded-md bg-blueFunko-700 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blueFunko-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blueFunko-800 easy-in-out duration-150 hover:scale-105 hover:delay-150"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+        <p class="text-sm text-center mt-10 text-gray-400">——— Connect With ———</p>
+
+        <div class="flex justify-center w-full items-center">
+          <button class="border-2 border-black/30 rounded-lg mt-6 py-2 px-12 hover:bg-gray-50">
+            <img src="../assets/img/logos/GoogleLogo.svg" alt="" />
+          </button>
+        </div>
+
+        <div class="text-sm text-center mt-10">
+          <a href="#" class="font-semibold text-blueFunko-700 hover:text-blueFunko-600"
+            >Forgot password?</a
+          >
+        </div>
+        <p class="mt-3 text-center text-sm text-gray-500">
+          Not a member yet?
+          {{ ' ' }}
+          <a href="#" class="font-semibold text-blueFunko-700 hover:text-blueFunko-600">Sign up</a>
+        </p>
+      </div>
+      <img
+        src="../assets/img/details/PointShape.svg"
+        alt=""
+        class="w-10 absolute bottom-1 left-1"
+      />
+      <img src="../assets/img/details/PointShape.svg" alt="" class="w-10 absolute top-1 right-1" />
+    </div>
+  </div>
+</template>
+
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ export default {
       backgroundImage: {},
       variants: {},
       fontFamily: {},
-      colors: {}
+      colors: {
+        blueFunko: {
+          50: '#edfbff',
+          100: '#d6f4ff',
+          200: '#b6edff',
+          300: '#83e4ff',
+          400: '#49d2ff',
+          500: '#1fb5ff',
+          600: '#0798ff',
+          700: '#017ced',
+          800: '#0865c5',
+          900: '#0e569a',
+          950: '#0e345d'
+        }
+      }
     },
     plugins: []
   }
