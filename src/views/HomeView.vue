a71a2ba58e002651708c49d1d1c9540f37931a11
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-09-19 12:25:44 +0200

    Cards and CardsCarrousel were fully developed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,19 @@
 <script setup>
-import CarrouselBanner from '@/components/HomePage/CarrouselBanner.vue';
-
+import CarrouselBanner from '@/components/HomePage/CarrouselBanner.vue'
+import CardsCarrousel from '@/components/Cards/CardsCarrousel.vue'
 </script>
 
 <template>
   <header>
-  <CarrouselBanner />
-</header>
+    <CarrouselBanner />
+  </header>
+  <main class="flex gap-x-8 flex-col justify-center py-16">
+    <div class="mx-auto max-w-full lg:mx-0 text-start px-24">
+      <h2 class="text-3xl font-bold tracking-tight text-dark sm:text-4xl">DISCOUNTED PRODUCTS</h2>
+      <p class="mt-2 text-lg leading-8 text-gray-600">
+        Funkos on sale! Grabs yours before they're gone.
+      </p>
+    </div>
+    <CardsCarrousel> </CardsCarrousel>
+  </main>
 </template>
