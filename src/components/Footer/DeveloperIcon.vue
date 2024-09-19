b68c45b8f344d9a45e7b2a2e362444ce539cf570
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-19 13:21:09 +0200

    Created Developers section, Developer icon component; imported developer icons

---
+++ src/components/Footer/DeveloperIcon.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <a :href="link" target="_blank" rel="noopener noreferrer">
+    <img
+      :src="icon"
+      :alt="alt"
+      class="w-full min-w-12 h-12 rounded-full hover:opacity-75 transition-opacity duration-200"
+    />
+  </a>
+</template>
---
+++ src/components/Footer/DevelopersSection.vue
@@ -0,0 +1,57 @@
+<script setup>
+import Icon from './DeveloperIcon.vue'
+
+const developers = [
+  {
+    name: 'Aitor',
+    icon: new URL('@/assets/img/DevevlopersIcons/aitor.jpeg', import.meta.url).href,
+    link: 'https://github.com/aitorgarciafernandez'
+  },
+  {
+    name: 'Bego',
+    icon: new URL('@/assets/img/DevevlopersIcons/bego.jpeg', import.meta.url).href,
+    link: 'https://github.com/begoblanco'
+  },
+  {
+    name: 'Diego',
+    icon: new URL('@/assets/img/DevevlopersIcons/diego.jpeg', import.meta.url).href,
+    link: 'https://github.com/diegofdez56'
+  },
+  {
+    name: 'Javier',
+    icon: new URL('@/assets/img/DevevlopersIcons/javier.jpeg', import.meta.url).href,
+    link: 'https://github.com/devjmv'
+  },
+  {
+    name: 'Olena',
+    icon: new URL('@/assets/img/DevevlopersIcons/olena.jpeg', import.meta.url).href,
+    link: 'https://github.com/OlenaAndrushchenko'
+  },
+  {
+    name: 'Vero',
+    icon: new URL('@/assets/img/DevevlopersIcons/vero.jpeg', import.meta.url).href,
+    link: 'https://github.com/Miharu669'
+  }
+]
+</script>
+
+<template>
+  <div class="md:col-span-5 flex flex-col justify-center gap-5">
+    <div class="mb-2">
+      <a href="#">
+        <img class="h-[104px] w-auto" src="@/assets/img/logos/WhiteLogo.svg" alt="" />
+      </a>
+    </div>
+
+    <h3 class="text-blueFunko-50 text-base font-semibold">DEVELOPED BY:</h3>
+    <div class="flex flex-wrap gap-4">
+      <Icon
+        v-for="(dev, index) in developers"
+        :key="index"
+        :icon="dev.icon"
+        :link="dev.link"
+        :alt="dev.name"
+      />
+    </div>
+  </div>
+</template>
