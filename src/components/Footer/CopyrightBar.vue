3a95927b068b9cd13beee07665f842e8730a0cbf
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-19 13:24:19 +0200

    Created CopyrightBar; Added footer layout with SubscribeForm, DevelopersSection, and CopyrightBar components; Added FooterComponent to App.vue

---
+++ src/components/Footer/CopyrightBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-slate-950 py-4">
+    <div class="max-w-[1300px] mx-auto px-5">
+      <span class="text-slate-400 text-sm">
+        © 2024 All rights reserved. Copying the site and information is prohibited.
+      </span>
+    </div>
+  </div>
+</template>
