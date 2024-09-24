91c09b934bb39e9c25461359f88f938fe53b7b54
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-24 14:42:40 +0200

    add Admin Layout view and admin routes to router with role-based authentication

--- src/router/index.js
+++ src/router/index.js
@@ -30,10 +30,33 @@ const router = createRouter({
       name: 'products',
       component: ProductView
     },
+    {
+      path: '/admin',
+      component: () => import('@/views/admin/AdminLayout.vue'),
+      // TODO uncomment later when login works
+      // meta: { requiresAdmin: true },
+      children: [
+        {
+          path: 'products',
+          name: 'admin-products',
+          component: () => import('@/views/admin/ProductsView.vue'),
+        },
+        {
+          path: 'orders',
+          name: 'admin-orders',
+          component: () => import('@/views/admin/OrdersView.vue'),
+        },
+        {
+          path: 'sales-billing',
+          name: 'admin-sales-billing',
+          component: () => import('@/views/admin/SalesBillingView.vue'),
+        },
+      ],
+    },
   ]
 })
 
-router.beforeEach(() => {
+router.beforeEach((to, from, next) => {
   const store = useAuthStore();
 
   if (localStorage.getItem("id") && store.user.id == "") {
@@ -44,6 +67,16 @@ router.beforeEach(() => {
     store.user.access_token = localStorage.getItem("access_token");
     store.user.refresh_token = localStorage.getItem("refresh_token");
   }
+
+    if (to.matched.some(record => record.meta.requiresAdmin)) {
+      if (store.user.isAuthenticated && store.user.role === 'ROLE_ADMIN') {
+        next();
+      } else {
+        next({ path: '/', query: { redirect: to.fullPath } });
+      }
+    } else {
+      next();
+    }
 
 });
 
