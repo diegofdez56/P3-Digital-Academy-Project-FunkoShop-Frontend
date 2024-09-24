1bd631874adbea1d628f512c4600387c30fb6e0e
devjmv <javier161890@gmail.com>
2024-09-24 13:28:50 +0200

    ready UserForm component

--- src/components/Form/UserForm.vue
+++ src/components/Form/UserForm.vue
@@ -8,12 +8,11 @@ const auth = useAuthStore()
 
 const firstName = ref('');
 const lastName = ref('');
-const email = ref('');
 const street = ref('');
 const city = ref('');
 const region = ref('');
 const postalCode = ref('');
-const country = ref('IL');
+const country = ref('');
 
 const address = ref(null);
 const subscribed = ref(false);
@@ -29,19 +28,23 @@ async function setProfile() {
 async function getPerfil() {
     const response = await store.getProfile(auth.user.access_token);
 
-    const splitPjoneNumber = response.phoneNumber.split('-');
-    const countryCodeResponse = splitPjoneNumber[0];
-    const rigthNumberResponse = splitPjoneNumber[1];
-    rigthNumber.value = rigthNumberResponse;
-    countryCode.value = countryCodeResponse;
+    if (response.phoneNumber) {
+        const splitPjoneNumber = response.phoneNumber.split('-');
+        const countryCodeResponse = splitPjoneNumber[0];
+        const rigthNumberResponse = splitPjoneNumber[1];
+        rigthNumber.value = rigthNumberResponse;
+        countryCode.value = countryCodeResponse;
+    }
     firstName.value = response.firstName;
     lastName.value = response.lastName;
-    //email.value = response.email;
     street.value = response.street;
     city.value = response.city;
     region.value = response.region;
     postalCode.value = response.postalCode;
-    country.value = response.country;
+    if(response.country)
+        country.value = response.country;
+    else
+        country.value = 'ES';
     address.value = response.address;
     subscribed.value = response.subscribed;
     shipping.value = response.shipping;
@@ -56,7 +59,8 @@ getPerfil()
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-                <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive the products.</p>
+                <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive the
+                    products.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="col-span-6 sm:col-span-3">
@@ -75,15 +79,6 @@ getPerfil()
                         <div class="mt-2">
                             <input v-model="lastName" type="text" name="lastname" id="lastname"
                                 autocomplete="family-name"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-
-                    <div class="col-span-6 sm:col-span-6">
-                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
-                            address</label>
-                        <div class="mt-2">
-                            <input v-model="email" id="email" name="email" type="email" autocomplete="email"
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                         </div>
                     </div>
@@ -269,7 +264,6 @@ getPerfil()
                         <div class="mt-2">
                             <select v-model="country" id="country"
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                <option value="">Select country</option>
                                 <option value="US">United States</option>
                                 <option value="CA">Canada</option>
                                 <option value="MX">Mexico</option>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import UserForm from '@/components/Form/UserForm.vue';
 </script>
 
 <template>
   <div class="about">
-    
     <h1>This is an about page</h1>
-    <UserForm />
   </div>
 </template>
 
