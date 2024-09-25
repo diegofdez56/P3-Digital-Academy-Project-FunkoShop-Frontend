5a88343d41cdbc9170311a1bbc03a97fe862c77f
devjmv <javier161890@gmail.com>
2024-09-25 16:14:36 +0200

    added badge to ProducModal

--- src/components/Cards/BadgeCard.vue
+++ src/components/Cards/BadgeCard.vue
@@ -9,11 +9,11 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="rounded-xl px-2.5 border border-transparent text-xs flex items-center text-white transition-all shadow-sm"
+  <div class="rounded-xl px-2.5 border border-transparent text-lg flex items-center text-white transition-all shadow-sm"
     :class="!props.isAvailable ? 'bg-slate-800' : (props.isDiscount ? 'bg-red-500' : (props.isNew ? 'bg-blueFunko-700' : ''))">
     <div v-if="!(props.isAvailable && !props.isDiscount && !props.isNew)">
       <p v-if="!props.isAvailable">Sold</p>
-      <p v-if="props.isAvailable && props.isDiscount" class="text-lg">{{ discount.percentage }}% off</p>
+      <p v-if="props.isAvailable && props.isDiscount" class="">{{ discount.percentage }}% off</p>
       <p v-if="props.isAvailable && !props.isDiscount && props.isNew">New</p>
     </div>
   </div>
