fa13b9e3018673ef4ab61a43ce49726a963f5205
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-09-27 14:16:31 +0200

    badge size adjusted

--- src/components/Cards/BadgeCard.vue
+++ src/components/Cards/BadgeCard.vue
@@ -4,13 +4,22 @@ const props = defineProps({
   isNew: Boolean,
   isDiscount: Boolean,
   discount: Object
-});
-
+})
 </script>
 
 <template>
-  <div class="rounded-xl px-2.5 border border-transparent text-lg flex items-center text-white transition-all shadow-sm"
-    :class="!props.isAvailable ? 'bg-slate-800' : (props.isDiscount ? 'bg-red-500' : (props.isNew ? 'bg-blueFunko-700' : ''))">
+  <div
+    class="rounded-xl px-2.5 border border-transparent text-md flex items-center text-white transition-all shadow-sm"
+    :class="
+      !props.isAvailable
+        ? 'bg-slate-800'
+        : props.isDiscount
+          ? 'bg-red-500'
+          : props.isNew
+            ? 'bg-blueFunko-700'
+            : ''
+    "
+  >
     <div v-if="!(props.isAvailable && !props.isDiscount && !props.isNew)">
       <p v-if="!props.isAvailable">Sold</p>
       <p v-if="props.isAvailable && props.isDiscount" class="">{{ discount.percentage }}% off</p>
