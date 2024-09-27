62d265dfd7781e022d3d375a05b6c44099a90514
Miharu669 <veronicadoelfuentes@gmail.com>
2024-09-27 09:27:49 +0200

    added language store

--- src/main.js
+++ src/main.js
@@ -3,13 +3,30 @@ import 'tailwindcss/tailwind.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
 
+const i18n = createI18n({
+    locale: 'en',
+    messages: {
+        en: {
+            message: {
+                hello: 'hello world'
+            }
+        },
+        sp: {
+            message: {
+                hello: 'hola mundo'
+            }
+        }
+    }
+})
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia(), i18n)
 app.use(router)
 
 app.mount('#app')
