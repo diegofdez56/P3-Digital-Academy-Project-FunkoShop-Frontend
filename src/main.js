144328e73ecef1f1f36f5414d6fb752ac889a26b
Miharu669 <veronicadoelfuentes@gmail.com>
2024-09-27 10:49:22 +0200

    added languages with i18n

--- src/main.js
+++ src/main.js
@@ -1,32 +1,17 @@
-import './assets/main.css'
-import 'tailwindcss/tailwind.css'
+import '@/assets/main.css';
+import 'tailwindcss/tailwind.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { createI18n } from 'vue-i18n'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import i18n from '@/language/language.js'; 
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const i18n = createI18n({
-    locale: 'en',
-    messages: {
-        en: {
-            message: {
-                hello: 'hello world'
-            }
-        },
-        sp: {
-            message: {
-                hello: 'hola mundo'
-            }
-        }
-    }
-})
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(i18n); 
+app.use(router);
 
-app.use(createPinia(), i18n)
-app.use(router)
-
-app.mount('#app')
+app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <script setup>
-import CarrouselBanner from '@/components/HomePage/CarrouselBanner.vue'
-import CardsCarrousel from '@/components/Cards/CardsCarrousel.vue'
+import { useI18n } from 'vue-i18n';
+import CarrouselBanner from '@/components/HomePage/CarrouselBanner.vue';
+import CardsCarrousel from '@/components/Cards/CardsCarrousel.vue';
 import NewArrivalsCarrousel from '@/components/Cards/NewArrivalsCarrousel.vue';
+
+const { t } = useI18n();
 </script>
 
 <template>
@@ -10,15 +13,15 @@ import NewArrivalsCarrousel from '@/components/Cards/NewArrivalsCarrousel.vue';
   </header>
   <main class="flex gap-x-8 flex-col justify-center py-16">
     <div class="mx-auto max-w-full lg:mx-0 text-start px-24">
-      <h2 class="text-3xl font-bold tracking-tight text-dark sm:text-4xl text-center">DISCOUNTED PRODUCTS</h2>
+      <h2 class="text-3xl font-bold tracking-tight text-dark sm:text-4xl text-center">{{ t('discountedProducts') }}</h2>
       <p class="mt-2 text-lg leading-8 text-gray-600 text-center">
-        Funkos on sale! Grabs yours before they're gone.
+        {{ t('discountedDescription') }}
       </p>
     </div>
     <CardsCarrousel /> 
     <div class="mx-auto max-w-full lg:mx-0 px-24 mt-10">
-      <h2 class="text-3xl font-bold tracking-tight text-dark sm:text-4xl text-center">NEW ARRIVALS</h2>
-      <p class="mt-2 text-lg leading-8 text-gray-600 text-center">Be the First to Own the Latest Funkos!</p>
+      <h2 class="text-3xl font-bold tracking-tight text-dark sm:text-4xl text-center">{{ t('newArrivals') }}</h2>
+      <p class="mt-2 text-lg leading-8 text-gray-600 text-center">{{ t('newArrivalsDescription') }}</p>
     </div>
     <NewArrivalsCarrousel />
   </main>
