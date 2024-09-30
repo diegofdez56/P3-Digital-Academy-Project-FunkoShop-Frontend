f50b7eb62264384c10e799d63dff2113e0ceb38c
devjmv <javier161890@gmail.com>
2024-09-30 21:23:57 +0200

    added admin categories route

---
+++ src/views/admin/CategoriesView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import AdminHeading from '@/components/AdminDashboard/AdminHeading.vue';
+import { CategoryStore } from '@/stores/category/CategoryStore';
+import { onMounted, ref } from 'vue';
+
+const listCategoryStore = CategoryStore();
+const listCategories = ref([]);
+
+async function getCategories() {
+  const response = await listCategoryStore.getCategories();
+  listCategories.value = [...response];
+}
+
+onMounted(async () => {
+  getCategories();
+});
+
+console.log(listCategories)
+</script>
+
+<template>
+    <div>
+        <AdminHeading title="Categories Management"/>
+        //aqui todo para configurar las categorías
+    </div>
+</template>
+  
+  
+  
+<style scoped>
+</style>
