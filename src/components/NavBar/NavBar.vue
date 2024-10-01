b48763e02d9014130d321189e73e7675363a5e0e
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-01 12:35:56 +0200

    developing btn

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { ref } from 'vue'
 import ShoppingCart from '../ShoppingCart/ShoppingCart.vue'
-import '@material-tailwind/html/scripts/popover.js';
+import '@material-tailwind/html/scripts/popover.js'
 import {
   Dialog,
   DialogPanel,
   PopoverGroup,
   TransitionChild,
-  TransitionRoot,
+  TransitionRoot
   /*   Popover,
     PopoverPanel,
     Tab,
@@ -18,9 +18,9 @@ import {
 } from '@headlessui/vue'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 import LanguageSelector from './LanguageSelector.vue'
+import SearchButton from './SearchButton.vue'
 import ProfileMenu from './ProfileMenu.vue'
 import { useI18n } from 'vue-i18n'
-
 
 const { t } = useI18n()
 
@@ -70,22 +70,37 @@ const open = ref(false)
   <div class="bg-white">
     <TransitionRoot as="template" :show="open">
       <Dialog class="relative z-40 lg:hidden" @close="open = false">
-        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
-          enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100"
-          leave-to="opacity-0">
+        <TransitionChild
+          as="template"
+          enter="transition-opacity ease-linear duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
           <div class="fixed inset-0 bg-black bg-opacity-25" />
         </TransitionChild>
 
         <div class="fixed inset-0 z-40 flex">
-          <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
-            enter-from="-translate-x-full" enter-to="translate-x-0"
-            leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0"
-            leave-to="-translate-x-full">
-            <DialogPanel class="relative flex w-full max-w-xs flex-col overflow-y-auto bg-slate-950 pb-12 shadow-xl">
+          <TransitionChild
+            as="template"
+            enter="transition ease-in-out duration-300 transform"
+            enter-from="-translate-x-full"
+            enter-to="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leave-from="translate-x-0"
+            leave-to="-translate-x-full"
+          >
+            <DialogPanel
+              class="relative flex w-full max-w-xs flex-col overflow-y-auto bg-slate-950 pb-12 shadow-xl"
+            >
               <div class="flex px-4 pb-2 pt-5">
-                <button type="button"
+                <button
+                  type="button"
                   class="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-white"
-                  @click="open = false">
+                  @click="open = false"
+                >
                   <span class="absolute -inset-0.5" />
                   <span class="sr-only">Close menu</span>
                   <XMarkIcon class="h-6 w-6" aria-hidden="true" />
@@ -159,7 +174,11 @@ const open = ref(false)
       <nav aria-label="Top" class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div>
           <div class="flex h-16 items-center">
-            <button type="button" class="relative rounded-md bg-white p-1 text-black lg:hidden" @click="open = true">
+            <button
+              type="button"
+              class="relative rounded-md bg-white p-1 text-black lg:hidden"
+              @click="open = true"
+            >
               <span class="absolute -inset-0.5" />
               <span class="sr-only">Open menu</span>
               <Bars3Icon class="h-6 w-6" aria-hidden="true" />
@@ -174,10 +193,16 @@ const open = ref(false)
 
             <PopoverGroup class="hidden lg:ml-8 lg:block lg:self-stretch">
               <div class="flex h-full space-x-8">
-                <a href="/home" class="flex items-center text-sm font-medium text-white hover:text-blueFunko-300">
+                <a
+                  href="/home"
+                  class="flex items-center text-sm font-medium text-white hover:text-blueFunko-300"
+                >
                   {{ t('home') }}
                 </a>
-                <a href="/products" class="flex items-center text-sm font-medium text-white hover:text-blueFunko-300">
+                <a
+                  href="/products"
+                  class="flex items-center text-sm font-medium text-white hover:text-blueFunko-300"
+                >
                   {{ t('products') }}
                 </a>
                 <!-- <Popover v-for="category in navigation.categories" :key="category.name" class="flex" v-slot="{ open }">
@@ -239,11 +264,12 @@ const open = ref(false)
             </PopoverGroup>
 
             <div class="ml-auto flex items-center">
+              <SearchButton />
               <LanguageSelector />
               <div class="ml-4 flow-root lg:ml-4 bg-white rounded-md">
                 <ShoppingCart />
               </div>
-              <div class="ml-4 flow-root lg:ml-4  text-white">
+              <div class="ml-4 flow-root lg:ml-4 text-white">
                 <ProfileMenu />
               </div>
             </div>
