f7ed775d69acce9d813da57edf14a022f0c1cc58
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-12 22:20:51 +0200

    i have to test something

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import ProductView from './../views/ProductView.vue';
-import { useAuthStore } from '@/stores/auth';
-import HomeView from '@/views/HomeView.vue';
+import ProductView from './../views/ProductView.vue'
+import { useAuthStore } from '@/stores/auth'
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -8,7 +8,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      redirect: "/home",
+      redirect: '/home'
     },
     {
       path: '/home',
@@ -19,7 +19,7 @@ const router = createRouter({
       path: '/checkout',
       name: 'checkout',
       component: () => import('@/views/CheckoutView.vue'),
-      meta: { requiresAuth: true },
+      meta: { requiresAuth: true }
     },
     {
       path: '/products',
@@ -30,19 +30,25 @@ const router = createRouter({
       path: '/profile',
       name: 'profile',
       component: () => import('@/views/user/ProfileView.vue'),
-      meta: { requiresAuth: true },
+      meta: { requiresAuth: true }
     },
     {
       path: '/favorite',
       name: 'favorite',
       component: () => import('@/views/user/FavoriteView.vue'),
-      meta: { requiresAuth: true },
+      meta: { requiresAuth: true }
     },
     {
       path: '/orders',
       name: 'orders',
       component: () => import('@/views/user/OrderView.vue'),
-      meta: { requiresAuth: true },
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/order/success',
+      name: 'order-success',
+      component: () => import('@/views/user/OrderSuccessView.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/admin',
@@ -52,66 +58,66 @@ const router = createRouter({
         {
           path: 'products',
           name: 'admin-products',
-          component: () => import('@/views/admin/ProductsView.vue'),
+          component: () => import('@/views/admin/ProductsView.vue')
         },
         {
           path: 'orders',
           name: 'admin-orders',
-          component: () => import('@/views/admin/OrdersView.vue'),
+          component: () => import('@/views/admin/OrdersView.vue')
         },
         {
           path: 'sales-billing',
           name: 'admin-sales-billing',
-          component: () => import('@/views/admin/SalesBillingView.vue'),
+          component: () => import('@/views/admin/SalesBillingView.vue')
         },
         {
           path: 'categories',
           name: 'admin-categories',
-          component: () => import('@/views/admin/CategoriesView.vue'),
-        },
-      ],
+          component: () => import('@/views/admin/CategoriesView.vue')
+        }
+      ]
     },
     {
       path: '/:pathMatch(.*)*',
-      redirect: "/404",
+      redirect: '/404'
     },
     {
       path: '/404',
-      name: 'NotFound', 
-      component: () => import('@/views/notFound/NotFoundView.vue'),
+      name: 'NotFound',
+      component: () => import('@/views/notFound/NotFoundView.vue')
     },
     {
       path: '/unsubscribe',
       name: 'Unsubscribe',
-      component: () => import('@/views/UnsubscribeView.vue'),
+      component: () => import('@/views/UnsubscribeView.vue')
     }
   ]
 })
 
 router.beforeEach((to) => {
-  const store = useAuthStore();
+  const store = useAuthStore()
 
-  if (localStorage.getItem("id") && store.user.id == "") {
-    store.user.id = localStorage.getItem("id");
-    store.user.email = localStorage.getItem("email");
-    store.user.role = localStorage.getItem("role");
-    store.user.isAuthenticated = localStorage.getItem("isAuthenticated") == "true" ? true : false;
-    store.user.access_token = localStorage.getItem("access_token");
-    store.user.refresh_token = localStorage.getItem("refresh_token");
-    store.user.lang = localStorage.getItem("lang");
+  if (localStorage.getItem('id') && store.user.id == '') {
+    store.user.id = localStorage.getItem('id')
+    store.user.email = localStorage.getItem('email')
+    store.user.role = localStorage.getItem('role')
+    store.user.isAuthenticated = localStorage.getItem('isAuthenticated') == 'true' ? true : false
+    store.user.access_token = localStorage.getItem('access_token')
+    store.user.refresh_token = localStorage.getItem('refresh_token')
+    store.user.lang = localStorage.getItem('lang')
   }
 
-  if (to.meta.requiresAdmin && !(store.user.role == "ADMIN")) {
+  if (to.meta.requiresAdmin && !(store.user.role == 'ADMIN')) {
     return {
-      path: "/home",
-    };
+      path: '/home'
+    }
   }
 
-  if (to.meta.requiresAuth && !store.user.isAuthenticated && !(store.user.role == "USER")) {
+  if (to.meta.requiresAuth && !store.user.isAuthenticated && !(store.user.role == 'USER')) {
     return {
-      path: "/home",
-    };
+      path: '/home'
+    }
   }
-});
+})
 
 export default router
